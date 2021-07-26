526e1d9aef12df840e16695f449bffc8c289d56a
Raghav Bhardwaj <raghavbhardwaj32@gmail.com>
2021-07-27 02:38:43 +0530

    update app with shopPage, collectionPreview component and CollectionItem component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import Homepage from "./page/homepage/homepage"
-import { Switch, Route } from "react-router-dom"
-
-const temp = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h3>You clicked</h3>
-      <ul>
-        <li><strong>pathname:</strong>{props.location.pathname}</li>
-        <li><strong>param:</strong>{props.match.params.item}</li>
-        <li><strong>path:</strong>{props.match.path}</li>
-        <li><strong>url:</strong>{props.match.url}</li>
-      </ul>
-    </div>
-  )
-}
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./page/homepage/homepage";
+import ShopPage from "./page/shop/Shop";
+import "./App.css"
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop/:item" component={temp} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
